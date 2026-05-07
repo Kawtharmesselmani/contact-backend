423131dd99766bb2fc61ffd68e6567669299680a
Kawtharmesselmani <kawtharmesselmani@gmail.com>
2026-05-07 20:27:35 +0300

    Connect backend to Railway DB

--- db.js
+++ db.js
@@ -1,18 +1,24 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// Test connection
+db.getConnection((err, connection) => {
   if (err) {
     console.error('DB connection error:', err);
   } else {
     console.log('Connected to Railway MySQL ✔');
+    connection.release();
   }
 });
 
